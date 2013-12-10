dee5b47e9e0d19bad2f5da655becc0dcd6780b1c
Sean Buscay <buscay@palantir.net>
2013-12-10 16:41:12 -0500

    Add rush recipe and templates.

---
+++ cookbooks/core/vdd/recipes/rush.rb
@@ -0,0 +1,34 @@
+git "/home/vagrant/.drush/rush" do
+    repository "https://github.com/palantirnet/rush.git"
+    action :sync
+end
+
+
+directory "/home/vagrant/Rush" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+directory "/home/vagrant/Rush/environment" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+directory "/home/vagrant/Rush/jobs" do
+  owner "vagrant"
+  group "vagrant"
+  mode "0755"
+  action :create
+end
+
+
+template "/home/vagrant/Rush/environment/params.ini" do
+  source "rush_params.erb"
+  mode "0755"
+  owner "vagrant"
+  group "vagrant"
+end
